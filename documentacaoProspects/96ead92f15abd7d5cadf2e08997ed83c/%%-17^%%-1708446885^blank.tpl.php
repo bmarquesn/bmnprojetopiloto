16bcd7ab610db7d9befc2ae46b35fdfd3dc4a8ea
Bruno Marques Nogueira <bmarquesn@gmail.com>
2016-04-23 10:39:14 -0300

    Ajustado gitignore

---
+++ documentacaoProspects/96ead92f15abd7d5cadf2e08997ed83c/%%-17^%%-1708446885^blank.tpl.php
@@ -0,0 +1,19 @@
+<?php /* Smarty version 2.6.0, created on 2015-03-23 23:42:26
+         compiled from blank.tpl */ ?>
+<html>
+	<head>
+		<title><?php echo $this->_tpl_vars['maintitle']; ?>
+</title>
+		<link rel="stylesheet" href="<?php echo $this->_tpl_vars['subdir']; ?>
+media/stylesheet.css" />
+		<meta http-equiv='Content-Type' content='text/html; charset=<?php echo $this->_tpl_vars['charset']; ?>
+'/>
+	</head>
+	<body>
+		<div align="center"><h3><?php echo $this->_tpl_vars['maintitle']; ?>
+</h3></div>
+		<p>Este é um CRUD, um sistema para controle de Prospects utilizando o Framework Codeigniter.</p>
+		<p>Feito em PHP e MySql. Utiliza JQuery, Ajax, Procedure, Trigger e Bootstrap.</p>
+		<p>Para mais informações consulte na pasta assets/docs o arquivo Bruno_Marques_Nogueira_Curriculo.doc</p>
+	</body>
+</html>
