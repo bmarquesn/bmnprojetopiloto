219c7a0e0d028da5521e8fb145ac680b65e1c878
Bruno Marques Nogueira <bmarquesn@gmail.com>
2018-02-15 15:13:19 -0200

    Update Admin.php
    Explicação do porque a Classe Comuns precisa sempre ser instanciada

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -1,6 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /** biblioteca padrão para todo o sistema */
+/** 15/02/2018 - Por estar dentro do PHP Query a integração com o Codeigniter, esta classe Comuns precisará sempre ser instanciada */
 require_once('Comuns.php');
 
 class Admin extends Comuns {
